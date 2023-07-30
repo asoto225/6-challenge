22b49f2b7266e5c6364a19cdbb92d15e40046461
asoto225 <135178400+abel25xD@users.noreply.github.com>
2023-07-30 15:00:40 -0700

    working api, updated html, starter style

--- assets/script.js
+++ assets/script.js
@@ -1,58 +1,60 @@
-// let cityName = document.getElementById('searchbar');
-let searchbtn = document.getElementById('search-btn');
-let searchBar = document.getElementById('searchbar');
+var searchbtn = document.getElementById('search-btn'); // search button
+var searchBar = document.getElementById('searchbar'); // search bar value
+var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=Tucson&appid=47fbf18e9aa6db4612259cb730822a98&units=imperial'; // api url
+var temp = document.querySelector('.temp'); // add temp value
+var windSpeed = document.querySelector('.wind-speed'); // add wind speed value
+var humidity = document.querySelector('.humidity'); // add humidity value
+var city = document.querySelector('.name'); // add city name
 
 
 document.addEventListener("DOMContentLoaded", function () {
     var currentDate = new Date();
     var options = { month: "long", day: "numeric", year: "numeric" };
     var formattedDate = currentDate.toLocaleDateString(undefined, options);
-
+    var currentWeatherItemsEl = document.getElementsByClassName('weather-items');
     document.getElementById("currentDay").textContent = formattedDate;
 
 });
 
-var response = fetch("https://api.openweathermap.org/data/2.5/forecast?q=Tucson&appid=47fbf18e9aa6db4612259cb730822a98&units=imperial")
-    .then(function (response) { return response.json() })
-    .then(
-        function(data) {
 
+
+function getApi(requestUrl) {
+    fetch(requestUrl)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
             console.log(data)
-            // let weatherData = {
-                // temperature: data.main.temp,
-                // humidity: list[0].main.humidity,
-                // wind: list[0].wind.speed,
+            var i = data.list[i]
+            var tempValue = data.list[0].main.temp;
+            var windValue = data.list[0].wind.speed;
+            var humidValue = data.list[0].main.humidity;
+            var cityName = data.city.name;
+            
+            var results = [
+            temp.innerHTML = tempValue,
+            windSpeed.innerHTML = windValue,
+            humidity.innerHTML = humidValue,
+            city.innerHTML = cityName
+            ]
+
+            for(i = 1; i < 6; i++) {
+                console.log(i)
+                if(i === 5)
+                return results
+            }
+
+            // temp.innerHTML = tempValue;
+            // windSpeed.innerHTML = windValue;
+            // humidity.innerHTML = humidValue;
+            // city.innerHTML = cityName;
+
+        });
+};
+
+getApi(requestUrl);
 
 
-        }
-           
-        
-    
-        
-    );
-    
-
-
-
-// function getWeatherData(cityName) {
-//     let url = "https://api.openweathermap.org/data/2.5/forecast?q=Tucson&appid=47fbf18e9aa6db4612259cb730822a98"
-//     return fetch(url)
-//     .then (response => response.json())
-//     .then (data => {
-//         const weatherData = {
-//             temp: data[0].main.temp,
-//             humidity: data[0].main.humidity,
-//             wind: data.wind[0].speed
-//         };
-//         console.log(weatherData);
-    
-//     });
-
-// };
-
-// searchbtn.addEventListener('click', () => 
-// {
-//     var location = searchBar.value;
-//     getWeatherData(location)
-//     .then(weatherData)
+// searchbtn.addEventListener('click', getApi, {
+//     // var inputValue = searchBar.ariaValueMax;
 // });
